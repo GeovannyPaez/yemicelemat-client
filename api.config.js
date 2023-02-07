7b31997cab20ebebb952f4bb2242e47f123e56fd
Geovanny Paez <yuniorpaezpabon@gmail.com>
2023-02-06 21:19:59 -0500

    change info of printReceipt

--- api.config.js
+++ api.config.js
@@ -1,3 +1,3 @@
-const URL_API= 'http://localhost:3000/api/v1/' /* 'https://whispering-journey-45701.herokuapp.com/api/v1/';*/
+const URL_API= /*'http://localhost:3000/api/v1/'*/ 'https://whispering-journey-45701.herokuapp.com/api/v1/'
 
 export {URL_API};
--- src/components/printReceipt/ComponentToPrint.jsx
+++ src/components/printReceipt/ComponentToPrint.jsx
@@ -11,7 +11,7 @@ class ComponetToPrint extends React.Component {
           <div className="row  text-center">
             <div className="col shadow-sm p-3  bg-body rounded">
               <h3>Multiservicios</h3>
-              <h4>La Cabaña</h4>
+              <h4>Yemicelemat</h4>
             </div>
           </div>
           <div className="row text-center">
@@ -19,7 +19,7 @@ class ComponetToPrint extends React.Component {
                 {this.props.children}
               </div>
           </div>
-          <p className="text-center">Observaciones, dudas e inquietudes a la linea +57 3138241259</p>
+          <p className="text-center">Observaciones, dudas e inquietudes a la linea +57 3145089366</p>
         </div>
       );
     }
