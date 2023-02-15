1badcf6f98a81adda995bf4eb18f757247e2f3f8
Geovanny Paez <yuniorpaezpabon@gmail.com>
2023-02-15 07:44:53 -0500

    add div.container to Route ContainerRecibos

--- src/components/iu/Recibos/TablasOFRecibos/ContainerRecibos.jsx
+++ src/components/iu/Recibos/TablasOFRecibos/ContainerRecibos.jsx
@@ -14,33 +14,82 @@ import { NotFountReceipts } from "../../NotFounts/NotFountReceipts";
 import TableRetiros from "./TableRetiros";
 import { AccordionInfoRetiros } from "../../../InfoReceipts/AccordionInfoRetiros";
 
-
 export default function ContainerRecibos() {
   const { type } = useParams();
-  const [ {receiptsAll,infoReceipts,loading},{getSearchById, getReceiptsByOptions} ] = useReceiptApi(type)
+  const [
+    { receiptsAll, infoReceipts, loading },
+    { getSearchById, getReceiptsByOptions },
+  ] = useReceiptApi(type);
   return (
-    <>
-    <div className="row align-items-center justify-content-between">
-      <Search getSearchById= {getSearchById}/> 
-      <div className="col-12 col-sm-6">
-        <SplitButtonFilter getReceiptsByOptions={getReceiptsByOptions}/>
+  
+      <div className="container m-auto p-1">
+        <div className="row align-items-center justify-content-between">
+          <Search getSearchById={getSearchById} />
+          <div className="col-12 col-sm-6">
+            <SplitButtonFilter getReceiptsByOptions={getReceiptsByOptions} />
+          </div>
+        </div>
+        <div className="table-responsive mb-5">
+          <table className="table caption-top table-striped table-hover">
+            <caption>{type.toUpperCase()}</caption>
+            {type === "celular" && (
+              <TableCelular
+                type={type}
+                receiptsAll={receiptsAll}
+                loading={loading}
+              />
+            )}
+            {type === "freefire" && (
+              <TableFreefire
+                type={type}
+                receiptsAll={receiptsAll}
+                loading={loading}
+              />
+            )}
+            {type === "pines" && (
+              <TablePines
+                type={type}
+                receiptsAll={receiptsAll}
+                loading={loading}
+              />
+            )}
+            {type === "venezuela" && (
+              <TableVenezuela
+                type={type}
+                receiptsAll={receiptsAll}
+                loading={loading}
+              />
+            )}
+            {type === "giros" && (
+              <TableGiros
+                type={type}
+                receiptsAll={receiptsAll}
+                loading={loading}
+              />
+            )}
+            {type === "consignaciones" && (
+              <TableConsignaciones
+                type={type}
+                receiptsAll={receiptsAll}
+                loading={loading}
+              />
+            )}
+            {type === "retiros" && (
+              <TableRetiros
+                loading={loading}
+                receiptsAll={receiptsAll}
+                type={type}
+              />
+            )}
+          </table>
+          {receiptsAll.length === 0 && <NotFountReceipts />}
+        </div>
+        {type === "retiros" ? (
+          <AccordionInfoRetiros loading={loading} infoReceipts={infoReceipts} />
+        ) : (
+          <AcordionInfo infoReceipts={infoReceipts} loading={loading} />
+        )}
       </div>
-    </div>
-    <div className="table-responsive mb-5">
-      <table className="table caption-top table-striped table-hover">
-        <caption>{type.toUpperCase()}</caption>
-        {type=== 'celular'&& <TableCelular  type={type} receiptsAll={receiptsAll} loading={loading}/>}
-        {type==='freefire'&& <TableFreefire type={type} receiptsAll={receiptsAll} loading={loading}/>}
-        {type==='pines'&& <TablePines type={type} receiptsAll={receiptsAll} loading={loading}/>}
-        {type==='venezuela'&& <TableVenezuela type={type} receiptsAll={receiptsAll} loading={loading}/>}
-        {type==='giros'&& <TableGiros type={type} receiptsAll={receiptsAll} loading={loading}/>}
-        {type==='consignaciones'&& <TableConsignaciones type={type} receiptsAll={receiptsAll} loading={loading}/>}
-        {type ==='retiros' && <TableRetiros loading={loading} receiptsAll={receiptsAll} type={type}/>
-        }
-      </table>
-      {receiptsAll.length ===0 && <NotFountReceipts/>}
-    </div>
-      {type === 'retiros' ?<AccordionInfoRetiros loading={loading} infoReceipts={infoReceipts}/> :<AcordionInfo infoReceipts={infoReceipts} loading={loading}/>}
-    </>
+    
   );
 }
